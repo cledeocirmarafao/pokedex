01bb8b454c2c8e62be65b3db3b2896075b936f16
Cledeocir Marafão <cledeocirms96@gmail.com>
2026-01-20 08:54:48 -0300

    feat(app): configure global providers and client-side routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import { PokemonCard } from "./components/PokemonCard"
-import { SplashScreen } from "./components/SplashScreen"
+import { Index } from "./components/pages/Index";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { usePokemonList } from "./components/hooks/usePokemon"
-import { usePokemonDetails } from "./components/hooks/usePokemon";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider } from "./components/contexts/ThemeContext";
+import { PokemonDetail } from "./components/pages/PokemonDetail";
+
+const queryClient = new QueryClient()
 
 function App(){
   return (
-    <>
-    <SplashScreen/>
-    </>
+    <QueryClientProvider client={queryClient}>
+    <ThemeProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/pokemon/:id" element={<PokemonDetail />} />
+          </Routes>
+        </BrowserRouter>
+    </ThemeProvider>
+  </QueryClientProvider>
   )
 }
 
