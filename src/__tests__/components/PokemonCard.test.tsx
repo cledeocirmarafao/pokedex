f82fe6fae3e407fdf09c6b72d000a26a65a99fb1
Cledeocir Marafão <cledeocirms96@gmail.com>
2026-01-23 06:23:45 -0300

    test(ui): implement unit tests for PokemonCard component

---
+++ src/__tests__/components/PokemonCard.test.tsx
@@ -0,0 +1,31 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import { describe, it, expect } from "vitest";
+import { PokemonCard } from "../../components/PokemonCard";
+import React from "react";
+
+const renderWithRoute = (component: React.ReactElement) => {
+  return render(<BrowserRouter>{component}</BrowserRouter>);
+};
+
+describe('PokemonCard Component', () => {
+    const mockPokemon = {
+        id:149,
+        name: 'dragonite',
+        image: 'https://example.com/dragonite.png',
+    }
+    
+    it('Renders Pokémon information correctly', () => {
+        renderWithRoute(<PokemonCard {...mockPokemon}/>)
+
+        expect(screen.getByText('dragonite')).toBeInTheDocument()
+        expect(screen.getByText('#149')).toBeInTheDocument()
+        expect(screen.getByAltText('dragonite')).toHaveAttribute('src', mockPokemon.image)
+    })
+
+    it('Navigate to the details page by clicking', () => {
+        renderWithRoute(<PokemonCard {...mockPokemon} id={9}/>)
+
+        expect(screen.getByText('#009')).toBeInTheDocument()
+    })
+})
