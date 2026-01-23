e8d95aca5ef45d8dc51f21c9466da5be15475ab2
Cledeocir Marafão <cledeocirms96@gmail.com>
2026-01-23 06:24:10 -0300

    test(ui): implement unit tests for SearchInput component

---
+++ src/__tests__/components/SearchInput.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { describe, it, expect, vi } from "vitest";
+import { SearchInput } from "../../components/SearchInput";
+
+describe("SearchInput Component", () => {
+  it("Calls onChange when the user types", async () => {
+    const user = userEvent.setup();
+    const onChange = vi.fn();
+
+    render(<SearchInput value="" onChange={onChange} />);
+    const input = screen.getByPlaceholderText(/Search Pokémon.../i);
+
+    await user.type(input, "Pikachu");
+
+    expect(onChange).toHaveBeenCalledTimes(7);
+  });
+
+  it("Displays the value controlled by props", () => {
+    const onChange = vi.fn();
+
+    render(<SearchInput value="Charizard" onChange={onChange} />);
+
+    const input = screen.getByDisplayValue('Charizard')
+    expect(input).toBeInTheDocument()
+  });
+});
